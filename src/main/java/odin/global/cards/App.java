6aefffe74e96598ce27841be4add1703188be5cb
Edison <edijs.britto@gmail.com>
2022-02-16 11:46:12 -0300

    novo

---
+++ src/main/java/odin/global/cards/App.java
@@ -0,0 +1,14 @@
+package odin.global.cards;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
---
+++ src/main/java/odin/global/cards/Greeter.java
@@ -0,0 +1,7 @@
+package odin.global.cards;
+
+public class Greeter {
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
